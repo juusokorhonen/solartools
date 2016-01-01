4afafa87dd9a422c6981f1035415e4d152b8aa8e
Juuso Korhonen <jk.github@turqoosi.net>
2016-01-01 20:23:25 +0200

    Reorganizing rest api

--- backend/rest/__init__.py
+++ backend/rest/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 from flask import Flask, Blueprint, current_app, request, render_template, redirect, flash, url_for, abort, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from jinja2 import TemplateNotFound
 import numpy as np
 import pygal
@@ -10,10 +10,9 @@ import pygal
 restapi_bp = Blueprint('restapi', __name__)
 restapi = Api(restapi_bp)
 
-class HelloWorld(Resource):
+class LocationCalculator(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'lat': request.args['lat'], 'lon': request.args['lon']}
 
-restapi.add_resource(HelloWorld, '/helloworld')
-
+restapi.add_resource(LocationCalculator, '/location')
 
